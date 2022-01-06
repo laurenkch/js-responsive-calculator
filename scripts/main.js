acd710442c462e999dd3de88ee95c2d0e8f6af75
laurenkch <lkoch879@gmail.com>
2022-01-06 13:26:43 -0500

    equal alert

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 const nums = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
+const e = document.querySelector('.equal-sign');
+console.log('e');
 
 function pushNumber (event) {
         alert(this.value);
@@ -16,3 +18,9 @@ function pushOperator (event) {
 operators.forEach(function (operator) {
     operator.addEventListener('click', this.pushOperator);
 })
+
+function calculate (event) {
+    alert("=");
+};
+
+e.addEventListener('click', this.calculate);
