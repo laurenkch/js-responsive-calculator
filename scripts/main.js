30300681dd7c3c9fdf67644acbb7c04028b38b70
laurenkch <lkoch879@gmail.com>
2022-01-06 20:10:49 -0500

    beginnings of the calculator functions

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ var calculation = [];
 //alerts commented out. 
 
 function pushNumber (event) {
-        // alert(this.value);
+        alert(this.value);
         calculation.push(this.value);
 };
 
@@ -16,7 +16,7 @@ nums.forEach(function (num) {
 });
 
 function pushOperator (event) {
-//         alert(this.value);
+        alert(this.value);
             calculation.push(this.value);
 };
 
@@ -24,10 +24,69 @@ operators.forEach(function (operator) {
     operator.addEventListener('click', this.pushOperator);
 })
 
-function calculate (event) {
-    alert("=");
+// function adds() {
+    
+// }
+function calculate (arr) {
+    let operators = "+-*/c"; 
+    let numbers = '123456789'
+    var empty = [];
+    var op = '';
+    var full = [];
+
+for (let i = 0; i < arr.length; i++) {
+    if (operators.includes(arr[i])) {
+        op = arr[i];
+        var full = empty;
+        var empty = [];
+    } else {
+            empty.push(arr[i]);
+    };
+if (op = "+"){
+    result = full.join + empty.join;
+    alert(result);
+} else if (op = "-") {
+    result = full.join - empty.join;
+    alert(result);
+} else if (op = "*") {
+    result = full.join * empty.join;
+    alert(result);
+}else if (op = "/") {
+    result = full.join/empty.join;
+    alert(result);
+}else if (op = "C") {
+    result = 0;
+    alert(result);
+}
+empty = [];
+op = '';
+full = [];
+
+}
+
+
+// console.log(arr);
 };
+
+
+
+
+
+//     let result = 0;
+//     if (calculation[i] === "+") {
+//      result = calculation[0] + calculation[2]; 
+//     } if (calculation[i] === "-") {
+//         result = calculation[0] - calculation[3];
+//     } if (calculation[i] === "*") {
+//         result = calculation[0] * calculation[3];
+//     } if (calculation[i] === "/") {
+//         result = calculation[0]/calculation[3];
+//     } if (calculation[i] === "C") {
+//         result = 0;
+//     }
+//     alert(result);
+// }
+// }
 equal.addEventListener('click', this.calculate);
 
-console.log(calculation);
-
+// console.log(calculation);
